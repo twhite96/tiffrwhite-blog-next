f0014f3695276f6d867e70aec7403d20a650ccdc
Tiffany White <twhite96@users.noreply.github.com>
2023-05-24 01:54:22 -0400

    footnote component

---
+++ components/footnotes.js
@@ -0,0 +1,16 @@
+<!-- Links -->
+<p>
+  <sup id="fnref:1">
+    <a href="#fn:1">1</a>
+  </sup>
+</p>
+
+<!-- Footnote List -->
+<div class="footnotes">
+  <ol>
+    <li class="footnote" id="fn:1">
+      <p>footnote. <a href="#fnref:1" title="return to article"> ↩</a></p>
+      <p></p>
+    </li>
+  </ol>
+</div>
