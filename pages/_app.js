f0d5cd9f901ec8a9ade608530c34a4977b627555
Tiffany <tiffany@tiffanywhite.dev>
2023-05-23 23:52:24 -0400

    Update something

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "nextra-theme-blog/style.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
--- pages/posts/[slug].js
+++
@@ -1,8 +0,0 @@
-import { getStaticPaths } from 'next/router'
-
-export const getStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: 'blocking'
-  }
-}
