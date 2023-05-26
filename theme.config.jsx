7b9611823c32d4882fc5276959c4535e71c636f4
Tiffany <tiffany@tiffanywhite.dev>
2023-05-26 18:03:22 -0400

    Config

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,16 +2,23 @@ export default {
   footer: <p>© tiff 2015 - {new Date().getFullYear()}</p>,
   head: ({ title, meta }) => (
     <>
-      {meta.description && (
-        <meta name='description' content={meta.description} />
-      )}
-      {meta.tag && <meta name='keywords' content={meta.tag} />}
-      {meta.author && <meta name='author' content={meta.author} />}
+      <meta name='robots' content='follow, index' />
+      <meta name='description' content={meta.description} />
+      <meta property='og:site_name' content={meta.title} />
+      <meta property='og:description' content={meta.description} />
+      <meta property='og:title' content={meta.title} />
+      <meta property='og:image' content={meta.image} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content={meta.title} />
+      <meta name='twitter:description' content={meta.description} />
+      <meta name='twitter:image' content={meta.image} />
     </>
   ),
   readMore: "Read More →",
   postFooter: null,
-  darkMode: false,
+  darkMode: true,
+  search: true,
+  primaryHue: 197,
   navs: [
     {
       url: "/index",
@@ -34,4 +41,5 @@ export default {
       name: "My stuff",
     },
   ],
+  logo: "https://res.cloudinary.com/twhiteblog/image/upload/v1685138249/favicon_zvkwc8.svg",
 };
