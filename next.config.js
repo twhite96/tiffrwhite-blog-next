d476016803f047a37b33bab5aeee6e95155bf9df
Tiffany <tiffany@tiffanywhite.dev>
2023-05-26 03:26:47 -0400

    jokpklopkmlp

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-import littlefoot from "littlefoot";
-import "littlefoot/dist/littlefoot.css";
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.jsx'
+})
 
-const withNextra = require("nextra")({
-  theme: "nextra-theme-blog",
-  themeConfig: "./theme.config.jsx",
-});
 
-module.exports = withNextra({
-  littlefoot: littlefoot
-});
-
+module.exports = withNextra()
+ 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
