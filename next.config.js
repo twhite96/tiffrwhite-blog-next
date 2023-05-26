13d8ccd3d6fbf4d5657bb3cb3acb0eb52c565ffc
Tiffany <tiffany@tiffanywhite.dev>
2023-05-26 03:20:44 -0400

    c

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const withNextra = require('nextra')({
 
 const { littlefoot } = require("littlefoot");
 
-// module.exports = littlefoot()
-module.exports = withNextra({
+module.exports = littlefoot({
   littlefoot: littlefoot(),
-})
+});
+module.exports = withNextra()
  
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
